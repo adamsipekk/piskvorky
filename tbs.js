80417980e9d50e3b09238ad59563d2c6cc4753fb
adam.sipekk <adam.sipek@performax.cz>
2022-03-25 14:38:01 +0100

    soft fix

--- tbs.js
+++ tbs.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const seats = document.querySelectorAll(".row .seat");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
@@ -57,8 +57,7 @@ movieSelect.addEventListener("change", e => {
 //seat click event
 container.addEventListener("click", e => {
   if (
-    e.target.classList.contains("seat") &&
-    !e.target.classList.contains("occupied")
+    e.target.classList.contains("seat")
   ) {
     e.target.classList.toggle("selected");
 
